252ef0ac819012fabc6cf359b4a7d423acfd3118
gurleenkaur0703 <gurleenkaur.gk03@gmail.com>
2024-06-14 20:42:03 +0530

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const initBalance = 1;
+//   const Assessment = await hre.ethers.getContractFactory("Assessment");
+//   const assessment = await Assessment.deploy(initBalance);
+//   await assessment.deployed();
+
+//   console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+const hre = require("hardhat");
+
+async function main() {
+  const ParkingLot = await hre.ethers.getContractFactory("ParkingLot");
+  const parkingLot = await ParkingLot.deploy();
+  await parkingLot.deployed();
+
+  console.log(`ParkingLot contract deployed to: ${parkingLot.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
