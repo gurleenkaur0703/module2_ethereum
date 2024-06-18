17a0279bdc78ea2ffd77a9f3cb9cad79fbcf08ed
gurleenkaur0703 <gurleenkaur.gk03@gmail.com>
2024-06-18 10:35:08 +0530

    ParkingLot.sol

--- contracts/ParkingLot.sol
+++ contracts/ParkingLot.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.8.9;
 
 contract ParkingLot {
     address public owner;
-    uint public totalSpaces = 5;
+    uint public totalSpaces = 5;  
     uint public totalCars = 0;
 
     constructor() {
         owner = msg.sender;
     }
-
     modifier onlyOwner() {
         require(msg.sender == owner, "Only Owner Can Access");
         _;
     }
-
-    mapping(uint => bool) public spaceAvailability;
+    mapping(uint => bool) public spaceAvailability;    
     mapping(uint => address) public carAtSpace;
 
     function parkCar(address _carAddress) public onlyOwner {
-        require(totalCars < totalSpaces, "No parking space available");
-        for (uint i = 1; i <= totalSpaces; i++) {
-            if (!spaceAvailability[i]) {
+        require(totalCars < 5, "no parking space available");
+       
+        for (uint i = 1; i <= 5; i++) {
+            if (spaceAvailability[i] == false) {
                 spaceAvailability[i] = true;
                 carAtSpace[i] = _carAddress;
                 totalCars++;
@@ -29,29 +28,26 @@ contract ParkingLot {
             }
         }
     }
-
     function removeCar(address _carAddress) public onlyOwner {
-        require(totalCars > 0, "No car to remove");
+        require(totalCars > 0, "no car to remove");
+        uint spaceOfCar = 1;
         bool carFound = false;
-        uint spaceOfCar;
 
-        for (uint i = 1; i <= totalSpaces; i++) {
+        for (uint i = 1; i <= 5; i++) {
             if (carAtSpace[i] == _carAddress) {
                 spaceOfCar = i;
                 carFound = true;
-                break;
             }
         }
-
-        require(carFound, "No car found!");
-
         carAtSpace[spaceOfCar] = address(0);
         spaceAvailability[spaceOfCar] = false;
         totalCars--;
-    }
 
-    function assertAvailability() public view {
-        assert(totalCars >= 0 && totalCars <= totalSpaces);
+        if (!carFound) {
+            revert("No car Found!");
+        }
+    }
+    function assertAvailabilty() public view {
+        assert(totalCars >= 0 && totalCars <= 5);
     }
 }
-
